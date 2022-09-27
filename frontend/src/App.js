9bd973e63af2fb7af10605762e019d7a943b1956
markoleptic <markalexcham@gmail.com>
2022-09-27 15:27:28 -0500

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+import React, { useState } from "react";
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  let homePage = async () => {
+      const response = await fetch("/");
+      const data = await response.json();
+      setItems(data);
+ }
   return (
     <div className="App">
+      <form onSubmit={homePage}>
+        <input
+          type="text"
+          value={items}
+          placeholder="Enter Tweet Here"
+        ></input>
+        <button type ="submit">Hi</button>
+      </form>
     </div>
   );
 }
