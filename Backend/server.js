c8cf23c65da2e9c8d853651ef5bb632cd4d574cf
markoleptic <markalexcham@gmail.com>
2022-09-27 13:44:57 -0500

    qve

--- Backend/server.js
+++ Backend/server.js
@@ -1,43 +1,40 @@
-// const express = require('express');
-// const mysql = require('mysql');
-// const Chart = require('chart.js');
-
-// const app = express();
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "cs3203",
-//     database: "test_db",
-//     port: "3306"
-// })
-
-// app.get('/',(req,res) => {
-
-// })
-
-// connection.connect((err) => {
-//     if (err) {
-//         throw err
-//     }
-//     else {
-//         console.log("Connected")
-//     }
-// })
-
-// connection.query('CREATE TABLE tabletest(id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, thing VARCHAR(255) NOT NULL)', (err,rows) => {
-//     if(err) {
-//         throw err
-//     }
-//     else {
-//         console.log("DATA SENT BOIS")
-//         console.log(rows)
-//     }
-// })
-
-
-
+const Joi = require('joi');
+const http = require('http');
+const https = require('https');
+const fs = require("fs");
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 5000;
-app.listen(port);
 
+// middleware
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('BeatShot website under construction');
+});
+
+// file location of private key
+var privateKey = fs.readFileSync( '/home/ec2-user/beatshot_gg/private.pem' );
+
+// file location of SSL cert
+var certificate = fs.readFileSync( '/home/ec2-user/beatshot_gg/certificate.pem' );
+
+var server_config = {
+    key : privateKey,
+    cert: certificate
+};
+
+var https_server = https.createServer(server_config, app).listen(443, function(err){
+    console.log("Node.js Express HTTPS Server Listening on Port 443");
+});
+
+var http_server = http.createServer(function(req,res){    
+    // 301 redirect (reclassifies google listings)
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80, function(err){
+    console.log("Node.js Express HTTPS Server Listening on Port 80");    
+});
+
+app.listen(port);
 console.log("App is listening on port " + port)
