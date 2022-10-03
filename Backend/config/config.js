61e7762054e3f7ba71ed80c9d0181ca381cae1a7
markoleptic <markalexcham@gmail.com>
2022-10-02 21:33:20 -0500

    sdf

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -20,5 +20,20 @@ module.exports = {
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
+  },
+  "live": {
+    "username": process.env.db_username,
+    "password": process.env.db_password,
+    "database": process.env.db_database,
+    "host": process.env.db_host,
+    "port": process.env.db_port,
+    "logging" : console.log,
+    "maxConcurrentQueries": 100,
+    "dialect": "mysql",
+    "dialectOptions": {
+      ssl: 'Amazon RDS'
+    },
+    "pool": { maxConnections: 5, maxIdleTime: 30},
+    "language": 'en',
   }
 }
