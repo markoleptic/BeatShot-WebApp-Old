211c05beb87e2a6c4ed5b892a6e3fe2fb0e9795e
markoleptic <markalexcham@gmail.com>
2022-10-23 11:57:40 -0500

    home page work

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
+import logo from "../images/Beatshot_logo_header.png";
+
 const Home = () => {
   return (
     <>
       <div className="flex-container">
         <div className="content">
+          <div className="home-header">
+            <img className="home-header-logo" src={logo} alt="logo"></img>
+          </div>
           <div className="content-main">
-            <div className="select-best-container">
+            <div className="centered-bordered-container padding-1rem">
               <div>
-                <p className="fs-200">
+                <p className="fs-200 text-center">
                   Welcome to the official website for Beatshot, the free-to-play
                   rhythm-based aim trainer coming soon to Steam. Until then, you
                   can download alpha versions on
                   <a
-                    className="fs-200 text-white link hover-blue"
+                    className="fs-200 text-white link hover-blue "
                     href="https://github.com/markoleptic/BeatShot">
                     &nbsp;Github
                   </a>
                   .
                 </p>
               </div>
-              <p className="fs-200">
+              <p className="fs-200 text-center">
                 To gain access to visual analysis for all your game modes and
                 songs played,
                 <Link
