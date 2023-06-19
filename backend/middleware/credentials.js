8abc111b1da0d3403a2c28e395661c78accec256
markoleptic <markalexcham@gmail.com>
2023-06-18 23:00:46 -0500

    a

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -1,11 +1,17 @@
 const allowedOrigins = require('../config/allowedOrigins');
 
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
+    if (req.originalUrl.startsWith('/api/sendfeedback')) {
+        // skip any /api/sendfeedback routes
+        next();
     }
-    next();
+    else {
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+            res.header('Access-Control-Allow-Credentials', true);
+        }
+        next();
+    }
 }
 
 module.exports = credentials;
--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -3,20 +3,29 @@ const jwt = require("jsonwebtoken");
 
 // used to validate every generic token
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  if (authHeader) {
-    const accessToken = authHeader.split(" ")[1];
-    if (accessToken === null) {
-      return res.sendStatus(401).json("Not Authenticated");
-    }
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res.sendStatus(403).json("Invalid token");
-      }
-      next();
-    });
+  if (req.originalUrl.startsWith("/api/sendfeedback")) {
+    // skip any /api/sendfeedback routes
+    next();
   } else {
-    res.status(401).json("Not Authenticated");
+    const authHeader = req.headers["authorization"];
+    if (authHeader) {
+      const accessToken = authHeader.split(" ")[1];
+      if (accessToken === null) {
+        return res.sendStatus(401).json("Not Authenticated");
+      }
+      jwt.verify(
+        accessToken,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+          if (err) {
+            return res.sendStatus(403).json("Invalid token");
+          }
+          next();
+        }
+      );
+    } else {
+      res.status(401).json("Not Authenticated");
+    }
   }
 };
 
